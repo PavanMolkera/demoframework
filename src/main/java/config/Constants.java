d9f513fbad8fcb44e32b1348cdeea5f2c6448d24
pavanshankar <pavanmolkera99@gmail.com>
2025-11-03 16:26:43 +0530

    first commit

--- src/main/java/config/Constants.java
+++ src/main/java/config/Constants.java
@@ -12,3 +12,4 @@ public class Constants {
     public static final String BROWSER = ConfigReader.getProperty("browser");
     public static final int IMPLICIT_WAIT = Integer.parseInt(ConfigReader.getProperty("implicitWait"));
 }
+//
